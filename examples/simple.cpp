25d704ff469ada09553405d3e4182fc43269b86f
Matthew Patrohay <matthewpatrohay@gmail.com>
2024-06-24 22:01:06 -0600

    Reorganized the simple example

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -4,14 +4,16 @@
 
 int main() {
     SpinCamera camera;
-    if (camera.Initialize()) {
-        camera.SetPixelFormat(SpinOption::PixelFormat::BayerRG8);
-        camera.StartAcquisition();
-        camera.CaptureImage("example_image.jpg");
-        camera.StopAcquisition();
-        camera.Shutdown();
-    } else {
-        std::cout << "Failed to initialize the camera." << std::endl;
+    if (!camera.Initialize()) {
+        std::cout << "Failed to initialize the camera. Exiting with Failure" << std::endl;
+        return 1; // Return failure
     }
+
+    camera.SetPixelFormat(SpinOption::PixelFormat::BayerRG8);
+    camera.StartAcquisition();
+    camera.CaptureImage("example_image.jpg");
+    camera.StopAcquisition();
+    camera.Shutdown();
+
     return 0;
 }
